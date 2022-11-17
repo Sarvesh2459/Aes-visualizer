2bd61f1f124a86258c361906f9509c54ea804ca7
Sakshisaraswat23 <sakshikaruna16@gmail.com>
2022-11-17 19:12:34 +0530

    today2

--- src/Table6.jsx
+++ src/Table6.jsx
@@ -25,7 +25,7 @@ function Table6(prop) {
           <th>{arr[6]}{arr[7]}</th>
         </tr>:
         <tr>
-          <th>{arr[0]}{arr[1]}</th>
+        <th>{arr[0]}{arr[1]}</th>
           <th>{arr[2]}{arr[3]}</th>
           <th>{arr[4]}{arr[5]}</th>
           <th>{arr[6]}{arr[7]}</th>
@@ -48,27 +48,27 @@ function Table6(prop) {
         {flag>=41 && flag<=48 ?<tr  className='table-glow'>
         <th>{arr[16]}{arr[17]}</th>
           <th>{arr[18]}{arr[19]}</th>
-          <th>{arr[20]}{arr[21]}</th>
-          <th>{arr[22]}{arr[23]}</th>
+          <th>{arr[21]}{arr[22]}</th>
+          <th>{arr[23]}{arr[24]}</th>
         </tr>:
         <tr>
-          <th>{arr[16]}{arr[17]}</th>
+        <th>{arr[16]}{arr[17]}</th>
           <th>{arr[18]}{arr[19]}</th>
-          <th>{arr[20]}{arr[21]}</th>
-          <th>{arr[22]}{arr[23]}</th>
+          <th>{arr[21]}{arr[22]}</th>
+          <th>{arr[23]}{arr[24]}</th>
         </tr>}
 
 
         {flag>=49 && flag<=56 ?<tr  className='table-glow'>
-        <th>{arr[24]}{arr[25]}</th>
-          <th>{arr[26]}{arr[27]}</th>
-          <th>{arr[28]}{arr[29]}</th>
+          <th>{arr[25]}{arr[26]}</th>
+          <th>{arr[27]}{arr[28]}</th>
+          <th>{arr[29]}{arr[30]}</th>
           <th>{arr[30]}{arr[31]}</th>
         </tr>:
         <tr>
-          <th>{arr[24]}{arr[25]}</th>
-          <th>{arr[26]}{arr[27]}</th>
-          <th>{arr[28]}{arr[29]}</th>
+          <th>{arr[25]}{arr[26]}</th>
+          <th>{arr[27]}{arr[28]}</th>
+          <th>{arr[29]}{arr[30]}</th>
           <th>{arr[30]}{arr[31]}</th>
         </tr>   
         }  
