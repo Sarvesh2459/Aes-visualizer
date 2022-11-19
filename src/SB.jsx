f7b1cd3627c482bd3f503a652a709cfd2a1e7e05
Garvit117 <garvitagrawal@iitbhilai.ac.in>
2022-11-19 10:31:43 +0530

    garvit is back

--- src/SB.jsx
+++ src/SB.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { useState } from "react";
 import Table from './Table';
 import {arko,sb1,mat,final} from './Data'
+import EastIcon from '@mui/icons-material/East';
 
 
 function SB() {
@@ -33,8 +34,8 @@ function SB() {
   // };
   return (
     <div className="App">
-       <p>ark : {arko}</p>
-       <p>sb: {sb1}</p>
+       {/* <p>ark : {arko}</p> */}
+       {/* <p>sb: {sb1}</p> */}
        <div style={{margin: "auto", padding: "1%"}}  >
        <button className={visible ?'button-18':'fadeOut3'} onClick={change}>
                 Sbox substitution
@@ -43,6 +44,9 @@ function SB() {
         <div style={{marginTop:"7%"}}>
       <div className={visible ?'fadeIn':'fadeOut1'} style={{marginLeft:"15%",float: "left", width: "40%"}} >
       <Table  name={arko}/>
+      </div>
+      <div className={visible ?'fadeOut':'fadeIn'}  style={{float: "left", width: "27%"}} >
+      <EastIcon sx={{marginTop: "40%"  ,fontSize:"80px", marginLeft :"165%"}}/>
       </div>
       <div className={visible ?'fadeOut':'fadeIn'} style={{marginLeft:"1%",float: "right" , width: "42%"}} >
       <Table name={sb1}/>
--- src/ShiftRows.jsx
+++ src/ShiftRows.jsx
@@ -6,7 +6,7 @@ import Table2 from './Table2';
 import Table3 from './Table3';
 import Table4 from './Table4';
 import Table5 from './Table5';
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import EastIcon from '@mui/icons-material/East';
 import {arko,sb1,mat,final} from './Data'
 
 function ShiftRows() {
@@ -21,32 +21,34 @@ function ShiftRows() {
   
   return (
     <div >
-    <p>shift row : {mat} </p>
+    {/* <p>shift row : {mat} </p> */}
     <h1 style={{marginLeft: "43%"}}>Shift Row</h1>
-    <div style={{marginLeft: "35%",marginTop: "4%"}}>    
-      {(flag>=1 && flag<3) && <div style={{padding:"2%"}} >
+    <div style={{marginLeft: "15%",marginTop: "4%"}}>    
+      {(flag>=1 && flag<3) && <div style={{marginLeft: "25%",padding:"2%"}} >
       <Table  name={sb1}/>
       </div>}
-      {(flag>=3 && flag<5) && <div style={{padding:"2%"}} >
+      {(flag>=3 && flag<5) && <div style={{marginLeft: "17%",padding:"2%"}} >
       <Table2 name={sb1}/>
       </div>}
-      {(flag>=5 && flag<7) && <div style={{padding:"2%"}} >
+      {(flag>=5 && flag<7) && <div style={{marginLeft: "17%",padding:"2%"}} >
       <Table3 name={sb1}/>
       </div>}
-      {(flag>=7 && flag<9) && <div style={{padding:"2%"}} >
+      {(flag>=7 && flag<9) && <div style={{marginLeft: "17%",padding:"2%"}} >
       <Table4 name={sb1}/>
       </div>}
-      {(flag>=9 && flag<11) && <div style={{padding:"2%"}} >
+      {(flag>=9 && flag<11) && <div style={{marginLeft: "25%",padding:"2%"}} >
       <Table5 name={mat}/>
       </div>}
       {(flag>=11) && <div style={{padding:"2%"}} >
+      <div  style={{float: "left", width: "27%"}} >
       <Table name={sb1}/>
-      <br/>
-      <br/>
-      <ArrowDownwardIcon sx={{fontSize:"80px", marginLeft :"15%"}}/>
-      <br/>
-      <br/>
-      <Table5 name={mat}/>
+      </div>
+      <div  style={{float: "left", width: "27%"}} >
+      <EastIcon sx={{marginTop: "40%"  ,fontSize:"80px", marginLeft :"40%"}}/>
+      </div>
+      <div  style={{float: "left", width: "27%"}} >
+      <Table5  name={mat}/>
+      </div>
       </div>}
     </div>
     </div>
